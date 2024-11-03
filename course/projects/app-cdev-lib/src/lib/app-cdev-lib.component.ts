9097c013c37a42d2953bc95020306784099637ef
Sergio Hidalgo <shidalgoc@auna.pe>
2024-11-03 07:16:01 -0500

    creating projects

---
+++ course/projects/app-cdev-lib/src/lib/app-cdev-lib.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cdevlib-appCDevLib',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      app-cdev-lib works!
+    </p>
+  `,
+  styles: ``
+})
+export class AppCDevLibComponent {
+
+}
