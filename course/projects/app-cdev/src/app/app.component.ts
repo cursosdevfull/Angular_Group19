d78716a21a0d154692ead001f5ab39228679ee6f
Sergio Hidalgo <shidalgoc@auna.pe>
2024-11-09 18:53:02 -0500

    login component

--- course/projects/app-cdev/src/app/app.component.ts
+++ course/projects/app-cdev/src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { LoginComponent } from './modules/auth/views/components/login/login.component';
+
 @Component({
   selector: 'cdev-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LoginComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'appCDev';
